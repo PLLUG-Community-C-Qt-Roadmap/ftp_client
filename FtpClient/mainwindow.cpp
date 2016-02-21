8ae3d963aecfb36caa08b9c8557f3b65a709fe86
irabokalo <iraklepak@gmail.com>
2016-02-21 15:11:02 +0200

    Download
    Don't work correctly

---
+++ FtpClient/mainwindow.cpp
@@ -0,0 +1,150 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QTcpSocket>
+
+#include <fstream>
+#include <QFile>
+
+const int portNumber = 1488;
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    socket = nullptr;
+
+    init();
+}
+
+void MainWindow::doConnect()
+{
+    if(!socket)
+    {
+        socket = new QTcpSocket(this);
+
+        connect(socket, &QAbstractSocket::connected,
+                this, &MainWindow::slotConnected);
+        connect(socket, static_cast<void(QTcpSocket::*)
+                (QAbstractSocket::SocketError)>(&QAbstractSocket::error),
+                this, &MainWindow::slotError);
+        connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::onChangeDir);
+        connect(ui->pushButton_2, &QPushButton::clicked, this, &MainWindow::onDownloadFile);
+        connect(ui->pushButton_3, &QPushButton::clicked, this, &MainWindow::onUploadFile);
+    }
+
+    qDebug() << "connecting...";
+
+    socket->connectToHost("localhost", portNumber);
+    socket->write("Hello");
+}
+
+void MainWindow::slotError(QAbstractSocket::SocketError err)
+{
+    switch(err)
+    {
+    case QAbstractSocket::HostNotFoundError :
+        qDebug() << "Error: The host was not found.";
+        break;
+    case QAbstractSocket::RemoteHostClosedError :
+        qDebug() << "Error: The remote host is closed.";
+        break;
+    case QAbstractSocket::ConnectionRefusedError :
+        qDebug() << "Error: The connection was refused.";
+        break;
+    default:
+        qDebug() << "The following error occured: " << socket->errorString();
+    }
+}
+
+void MainWindow::onChangeDir()
+{
+    std::string buffer;
+
+    buffer.resize(2);
+
+    *reinterpret_cast<short*>(&buffer[0]) = 0;
+
+    buffer += "Change dir plz";
+
+    socket->write( buffer.c_str(), buffer.size() );
+}
+
+void MainWindow::onDownloadFile()
+{
+
+    std::string buffer;
+
+    buffer.resize(6);
+
+    buffer[0] = static_cast<char>(1);
+    buffer[1] = static_cast<char>(1);
+    int *pointer = reinterpret_cast<int *>(&buffer[2]);
+    *pointer = 5;
+    buffer += "D:/tee.txt";
+    socket->write( buffer.c_str(), buffer.size() );
+
+    QFile saveMe ("my_file.txt");
+
+    QByteArray data;
+    socket->waitForReadyRead();
+
+    data = socket->readAll();
+    int Numb = static_cast<short>(data[0]);
+    int errorCode = static_cast<short> (data[1]);
+    int* ptr = reinterpret_cast<int *>(data.data()+2);
+    int addData = *ptr;
+     saveMe.open(QIODevice::WriteOnly);
+    while( addData> 0 )
+    {
+        socket->waitForReadyRead();
+        data = socket->readAll();
+        qDebug() << data;
+        saveMe.write(data.data()+6, 1024);
+        addData-= 1024;
+    }
+    qDebug()<< Numb<<" "<< errorCode << addData<<endl;
+   // saveMe.open(QIODevice::WriteOnly);
+
+   // saveMe.write(data);
+
+}
+
+void MainWindow::onUploadFile()
+{
+
+    /*QFile readStream("D:/AQDrj9BP5lk.jpg");
+
+    if( !readStream.open( QIODevice::ReadOnly ) )
+    {
+        qDebug() << "Can`t open file";
+        return;
+    }
+
+    qDebug() << readStream.size();
+
+    QByteArray data( 2, '0' );
+
+    *reinterpret_cast<short*>( data.data() ) = 2;
+
+    data.append( readStream.readAll() );
+
+    socket->write( data.data(), data.size() );*/
+}
+
+void MainWindow::slotConnected()
+{
+    qDebug() << "connected...";
+}
+
+void MainWindow::init()
+{
+    connect(ui->connectButton, &QPushButton::clicked,
+            this, &MainWindow::doConnect);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
--- FtpServer/Main.cpp
+++ FtpServer/Main.cpp
@@ -5,18 +5,126 @@
 #include <thread>
 #include <utility>
 #include <boost/asio.hpp>
-
+#include <boost/filesystem.hpp>
+#include <fstream>
+using namespace boost::filesystem;
 using boost::asio::ip::tcp;
 
 const int max_length = 1024;
+
+struct Executor
+{
+	static void ChangeDir(tcp::socket &sock)
+	{
+		boost::system::error_code error;
+		std::vector<char> data(max_length);
+		size_t length = sock.read_some(boost::asio::buffer(data), error);
+		data[length] = '\0';
+
+		std::cout << "Dispathed change dir" << std::endl;
+		std::cout << &data[0] << std::endl;
+	}
+
+	static void DownloadFile(tcp::socket &sock)
+	{
+		boost::system::error_code error;
+		std::vector<char> data(max_length);
+		size_t length = sock.read_some(boost::asio::buffer(data), error);
+		
+		data[length] = '\0';
+
+		short errorCode = static_cast<short>(data[0]);
+
+		int* pointer = reinterpret_cast<int*>(&data[1]);
+		int addData = *pointer;
+		
+		std::string filePath(data.begin() + 5, data.begin() + length);
+		int size = 0;
+		path Path(filePath);
+		if (exists(Path))    // does path p actually exist?
+		{
+			if (is_regular_file(Path))        // is path p a regular file?
+			{
+				size = file_size(Path); std::cout << file_size(Path) << '\n'; std::cout << Path << std::endl;
+			}
+			else if (is_directory(Path))      // is path p a directory?
+				std::cout << Path << " is a directory\n";
+			else
+				std::cout << Path << " exists, but is not a regular file or directory\n";
+		}
+		else
+			std::cout << Path << " does not exist\n";
+		ifstream file(filePath, std::ios::binary);
+		std::vector<char> array1;
+		array1.resize(1030);
+		array1[0] = static_cast<char>(3);
+		array1[1] = static_cast<char>(0);
+		int* newArrPointer = reinterpret_cast<int*> (&array1[2]);
+		*newArrPointer = size;
+		
+		int count = 0;
+		
+		while (size > 0)
+		{
+			file.read(&array1[6], 1024);
+			size_t l = sock.write_some(boost::asio::buffer(array1, array1.size()));
+			size -= max_length;
+		}
+	//	std::cout << "Dispathed download file" << std::endl;
+	//	std::cout << errorCode << " " << addData << std::endl;
+	}
+
+	static void UploadFile(tcp::socket &sock)
+	{
+		boost::system::error_code error;
+		std::vector<char> data(6000);
+		size_t length = sock.read_some(boost::asio::buffer(data), error);
+
+		std::cout << "Dispathed upload file" << std::endl;
+
+		std::ofstream file("myImg.png", std::ios::binary);
+
+		file.write(&data[0], length);
+	}
+};
+
+struct Dispather
+{
+	Dispather(tcp::socket &socket) : _socket(socket)
+	{
+	}
+	void Dispatch(short op)
+	{
+		if (op < 0 || op > 2)
+		{
+			std::cout << "Unknown operation" << std::endl;
+			return;
+		}
+
+		_operations[static_cast<MessageT>(op)](_socket);
+	}
+
+	enum class MessageT { ChangeDir = 0, DownloadFile = 1, UploadFile = 2 };
+	typedef std::function<void(tcp::socket&)> Action;
+	static std::map < MessageT, Action > _operations;
+
+	tcp::socket &_socket;
+};
+
+std::map<Dispather::MessageT, Dispather::Action> Dispather::_operations{
+	{ MessageT::ChangeDir, Executor::ChangeDir },
+	{ MessageT::DownloadFile, Executor::DownloadFile },
+	{ MessageT::UploadFile, Executor::UploadFile }
+};
 
 void session(tcp::socket sock)
 {
 	try
 	{
+		auto dispatcher = std::make_unique<Dispather>(sock);
 		for (;;)
 		{
-			char data[max_length];
+			char data[1];
 
 			boost::system::error_code error;
 			size_t length = sock.read_some(boost::asio::buffer(data), error);
@@ -25,7 +133,10 @@ void session(tcp::socket sock)
 			else if (error)
 				throw boost::system::system_error(error); // Some other error.
 
-			boost::asio::write(sock, boost::asio::buffer(data, length));
+
+//	short op = *reinterpret_cast<short*>(data);
+			short op = static_cast<short>(data[0]);
+			dispatcher->Dispatch(op);
 		}
 	}
 	catch (std::exception& e)
@@ -49,20 +160,14 @@ int main(int argc, char* argv[])
 {
 	try
 	{
-		if (argc != 2)
-		{
-			std::cerr << "Usage: blocking_tcp_echo_server <port>\n";
-			return 1;
-		}
-
 		boost::asio::io_service io_service;
-
-		server(io_service, std::atoi(argv[1]));
+		server(io_service, 1488);
 	}
 	catch (std::exception& e)
 	{
 		std::cerr << "Exception: " << e.what() << "\n";
 	}
 
+	system("pause");
 	return 0;
 }
